1311cf15fbccdae065f46643a4900505d7342b35
emreekmekcioglu1 <62750686+emreekmekcioglu1@users.noreply.github.com>
2021-08-27 12:54:28 +0300

    Add files via upload

---
+++ reflected_xss_html_context/reflected_xss_html_front.php
@@ -0,0 +1,19 @@
+<?php  
+session_start();
+setcookie("cookie_name");
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>SEARCH BY NAME</title>
+</head>
+<body style="margin-top: 100px; text-align: center;">
+<h1>Merhaba, bu site en basit hali ile reflected XSS tespiti yapılması için kurulmuştur.</h1> <!--In English: Hello, this site is created to see reflected XSS in basics. -->
+<form action="reflected_xss_html_processor.php" method="GET">
+	<input id = "searched" name = "search_word" type="text" placeholder="Kullanıcı ara"> <!-- Kullanıcı ara means Search User in Turkish-->
+	<input type="submit" name="Ara"> <!-- Ara means seach in Turkish-->
+</form>
+</body>
+</html>
